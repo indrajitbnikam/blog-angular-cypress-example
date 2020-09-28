d7cd06d6540942335c342b714a220d55826bbaf6
Indrajeet Nikam <indrajeet.nikam@siemens.com>
2020-09-28 23:25:09 +0530

    feat: initial draft

---
+++ cypress/integration/spec.ts
@@ -0,0 +1,14 @@
+it('loads examples', () => {
+  const baseUrl: string = 'http://localhost:4200';
+  cy.visit(baseUrl);
+  cy.contains('Here are some links to help you get started');
+});
+
+it('first test', () => {
+  const baseUrl = 'http://localhost:4200';
+  cy.visit(baseUrl);
+  cy.contains('New Component').click();
+  cy.get('.terminal > pre').should('contain.text', 'ng generate component xyz');
+  cy.contains('Angular Material').click();
+  cy.get('.terminal > pre').should('contain.text', 'ng add @angular/material');
+});
